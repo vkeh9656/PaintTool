16bf57b864beacc54ee16e3a117ccece899a5604
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-01-28 22:55:58 +0900

    라디오 버튼 추가

--- resource.h
+++ resource.h
@@ -5,6 +5,9 @@
 #define IDD_PAINTTOOL_DIALOG            102
 #define IDR_MAINFRAME                   128
 #define IDC_DRAW_RECT                   1000
+#define IDC_PEN_RADIO                   1001
+#define IDC_LINE_RADIO                  1002
+#define IDC_RECT_RADIO                  1003
 
 // Next default values for new objects
 // 
@@ -12,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
